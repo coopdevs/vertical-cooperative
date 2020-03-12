43723447e80b1245dcb95f67b5fc89aa523958fa
enricostano <enricostn@gmail.com>
2020-03-12 17:12:46 +0100

    Add customizations for company cooperators

--- easy_my_coop_es/__manifest__.py
+++ easy_my_coop_es/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Easy My Coop Spain",
-    'version': '12.0.0.0.4',
+    'version': '12.0.0.0.5',
     'depends': [
         'easy_my_coop',
         'easy_my_coop_website'
@@ -13,6 +13,7 @@
     """,
     "license": "AGPL-3",
     'data': [
+        "views/become_company_cooperator_view.xml",
         "views/become_cooperator_view.xml",
         "views/subscription_request_view.xml",
     ],
